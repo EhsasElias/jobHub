d705a320ea4422f93556aeaa43e193d5f5bf1006
Ehsas <ehsas.1998000@gmail.com>
2022-02-02 00:08:15 +0300

    filter edit

--- assetes/js/app.js
+++ assetes/js/app.js
@@ -18,8 +18,9 @@
 
 var _bgColor = document.getElementById('nav');
 var _signIn = document.getElementById('signIn');
-window.addEventListener('scroll' ,()=>{
-    if(window.scrollY < 50){
+window.addEventListener('scroll', () => {
+
+    if (window.scrollY < 50) {
         _bgColor.classList.remove('bg-white');
         _bgColor.classList.remove('shadow-lg');
         _signIn.classList.add('border-primary');
@@ -27,7 +28,7 @@ window.addEventListener('scroll' ,()=>{
         _signIn.classList.remove('border-dark');
         _signIn.classList.remove('text-dark');
     }
-   else if(window.scrollY >= 50){
+    else if (window.scrollY >= 50) {
         _bgColor.classList.add('bg-white');
         _bgColor.classList.add('shadow-lg');
         _signIn.classList.add('border-dark');
@@ -35,8 +36,14 @@ window.addEventListener('scroll' ,()=>{
         _signIn.classList.remove('border-primary');
         _signIn.classList.remove('text-primary');
     }
-    
-     
-   
-    
+
+});
+$(document).ready(function () {
+$('#categories').on('change', function () {
+    $('#com-name').empty();
+
+    if ($("#categories").val() == "com") { $("#com-name").append("<option>IOM</option><option>UN</option>").fadeIn(2000); }
+    else if ($("#categories").val() == "city") { $("#com-name").append("<option>Sanaa</option><option>Taiz</option><option>Hadermoot</option>").fadeIn(2000); }
+
+});
 });
